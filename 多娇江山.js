23b43fdda1398b535e2b238740c81515cfaad7ff
lwzlinyi <44020562+lwzlinyi@users.noreply.github.com>
2023-02-22 17:33:28 +0800

    Update 多娇江山.js

--- 多娇江山.js
+++ 多娇江山.js
@@ -507,7 +507,7 @@
   * 助力
   */
  async function invite() {
-   let body = `ref_code=43RZX9`
+   let body = `ref_code=V7NHR9`
    return new Promise(resolve => {
      $.post((taskPostUrl("/api/account/update_ref_code", body)), async (err, resp, data) => {
        try {
